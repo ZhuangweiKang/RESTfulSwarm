ece2a56c590c66927df60389bb38b4b1a30d6409
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-06-25 13:20:59 -0500

    Update

--- JobManager/JobManager.py
+++ JobManager/JobManager.py
@@ -8,7 +8,7 @@ import requests
 import argparse
 import json
 import threading
-from Scheduler import Scheduler
+import Scheduler
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
