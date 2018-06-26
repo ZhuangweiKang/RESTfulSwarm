ee9a2155eb7870a0851c8c2111db0f795d8be3e6
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-06-26 13:54:19 -0500

    Update

--- utl.py
+++ utl.py
@@ -148,7 +148,7 @@ def get_total_cores():
 def get_total_mem():
     meminfo = open('/proc/meminfo').read()
     memfree = re.search("MemFree:\s+(\d+)", meminfo)
-    memfree = memfree.group(0).split()[1]
+    memfree = str(memfree.group(0).split()[1]) + 'k'
     return str(memory_size_translator(memfree)) + 'm'
 
 
