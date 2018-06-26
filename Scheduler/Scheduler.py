8c4e62688d4dc41ff852ab50a0a35f14bc909902
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-06-26 14:46:04 -0500

    Update

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -69,11 +69,11 @@ class Scheduler:
 
             # add node filed
             target = 'job_info.tasks.%s.node' % item[0]
-            job_col.update({job_filter: item[0]}, {target: item[1]}, upsert=True)
+            job_col.update({job_filter: item[0]}, {'$set': {target: item[1]}}, upsert=True)
 
             # add cpuset_cpus field
             target = 'job_info.tasks.%s.cpuset_cpus' % item[0]
-            job_col.update({job_filter: item[0]}, {target: ','.join(item[2])}, upsert=True)
+            job_col.update({job_filter: item[0]}, {'$set': {target: ','.join(item[2])}}, upsert=True)
 
     def update_workers_info(self, resource_check_result):
         for item in resource_check_result:
