4d914c7113b9520eb80ed7796c403000b1c37fc7
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-07-09 23:06:23 -0500

    Update Discovery

--- Discovery/Discovery.py
+++ Discovery/Discovery.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import MongoDBHelper as mg
 import ZMQHelper as zmq
 import json
+import utl
 import argparse
 
 
@@ -18,10 +19,12 @@ class Discovery:
         self.db = mg.get_db(db_client, 'RESTfulSwarmDB')
         self.workers_info = mg.get_col(self.db, 'WorkersInfo')
         self.socket = zmq.csBind(discovery_port)
+        self.logger = utl.doLog('DiscoveryLogger', 'DiscoveryLog.log')
 
     def discovery(self):
         while True:
             msg = self.socket.recv_string()
+            self.logger.info('Recv msg: %s' % msg)
             self.socket.send_string('Ack')
             worker_host = msg.split()[0]
             msg = msg.split()[1]
@@ -33,18 +36,21 @@ class Discovery:
             filter_key = 'job_info.tasks.%s.container_name' % task_name
             target_key = 'job_info.tasks.%s.status'
             mg.update_doc(job_col, filter_key, task_name, target_key, 'Down')
+            self.logger.info('Updating Job collection.')
 
             # get the resource utilization of the 'Down' container
             job_info = mg.find_col(job_col)[0]
             cores = job_info['job_info']['tasks'][task_name]['cpuset_cpus']
             cores = cores.split(',')
             memory = job_info['job_info']['tasks'][task_name]['mem_limit']
+            self.logger.info('Collecting resources from down containers.')
 
             # update WorkersInfo collection
             # update cores info
             for core in cores:
                 target_key = 'CPUs.%s' % core
                 mg.update_doc(self.workers_info, 'hostname', worker_host, target_key, False)
+            self.logger.info('Updating core resources in WorkersInfo collection.')
 
             # update memory info
             worker_info = mg.filter_col(self.workers_info, 'hostname', worker_host)
@@ -54,6 +60,7 @@ class Discovery:
             updated_memory = memory + free_memory
             updated_memory = str(updated_memory) + 'm'
             mg.update_doc(self.workers_info, 'hostname', worker_host, 'MemFree', updated_memory)
+            self.logger.info('Updating memory resources in WorkersInfo collection.')
 
 
 if __name__ == '__main__':
@@ -71,4 +78,6 @@ if __name__ == '__main__':
     mongo_port = data['mongo_port']
 
     discovery = Discovery(mongo_addr, mongo_port)
+    discovery.logger.info('Initialized Discovery block.')
+
     discovery.discovery()
