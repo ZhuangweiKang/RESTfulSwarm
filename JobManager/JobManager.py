9cf8bbfb0a439a6566aa7c8720b07cdce876782c
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-07-12 11:22:48 -0500

    Update

--- JobManager/JobManager.py
+++ JobManager/JobManager.py
@@ -254,6 +254,9 @@ class JobManager:
                         temp_job_queue.append(msg)
                         job_queue.remove(msg)
 
+                    print('Job details: ')
+                    print(jobs_details)
+
                     schedule_resource(jobs_details)
 
                     for msg in temp_job_queue:
