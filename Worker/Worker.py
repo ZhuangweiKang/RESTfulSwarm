25cdd4c323a27b371bd0e57696f99eacb5a69c6b
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-07-13 15:25:44 -0500

    Update

--- Worker/Worker.py
+++ Worker/Worker.py
@@ -51,7 +51,10 @@ class Worker:
                 if event['Type'] == 'container' and \
                         (event['status'] == 'stop' or event['status'] == 'destroy' or event['status'] == 'die'):
                     msg = hostname + ' ' + event['Actor']['Attributes']['name']
-                    msgs.append(msg)
+                    if event['Actor']['Attributes']['name'] in self.storage.keys():
+                        msgs.append(msg)
+                        # remove the container form local storage
+                        del self.storage[event['Actor']['Attributes']['name']]
 
             # 去重
             msgs = list(set(msgs))
@@ -88,7 +91,6 @@ class Worker:
                     thr.start()
             elif msg_type == 'migrate':
                 info = json.loads(' '.join(msg[1:]))
-                print(info)
                 dst = info['dst']
                 container = info['container']
                 container_info = info['info']
