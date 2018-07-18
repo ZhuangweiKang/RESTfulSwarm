21dc09fe0f58a50bc7e196ff67f40aa3ad61b7f6
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-07-18 16:57:20 -0500

    update job status

--- Client/Client.py
+++ Client/Client.py
@@ -4,10 +4,130 @@
 
 import requests
 import argparse
+import random
+import pandas as pd
 import json
 
 fe_addr = None
 fe_port = None
+
+
+class Task:
+    def __init__(self, container_name, image, cpu_count, mem_limit, command=""):
+        self.container_name = container_name
+        self.image = image
+        self.command = command
+        self.cpu_count = cpu_count
+        self.mem_limit = mem_limit
+
+    def get_task(self):
+        task = {"container_name": self.container_name,
+                "node": "",
+                "image": self.image,
+                "detach": True,
+                "command": self.command,
+                "cpu_count": self.cpu_count,
+                "cpuset_cpus": "",
+                "mem_limit": self.mem_limit,
+                "ports": {},
+                "volumes": {},
+                "environment": {},
+                "status": "Ready"}
+        return task
+
+
+class StressClient:
+    def __init__(self, subnet, image_name, workload):
+        self.subnet = subnet
+        self.image_name = image_name
+        self.workload = workload
+
+    def generate_job(self, job_name, tasks_count):
+        with open('SampleJob.json', 'r') as f:
+            job = json.load(f)
+        job['job_name'] = job_name
+        job['job_info']['network']['subnet'] = self.subnet
+        for i in range(tasks_count):
+            task_name = job_name + '_task' + str(i+1)
+            image = self.image_name
+            cpu_count = random.randint(2, 6)
+            mem_limit = str(random.randint(10, 50)) + 'm'
+            task = Task(task_name, image, cpu_count, mem_limit)
+            task = task.get_task()
+            job['job_info']['tasks'].update({task_name: task})
+        return job
+
+    def steady(self):
+        jobs_data = {'time': [], 'job_count': []}
+        time = 0
+        while time < 200:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(3)
+            time += 10
+        df = pd.DataFrame(jobs_data, columns=['time', 'job_count'])
+        df.to_csv('steady.csv')
+
+    def bursty(self):
+        jobs_data = {'time': [], 'job_count': []}
+        time = 0
+        while time < 40:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(2)
+            time += 10
+        while time < 60:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(0.9*time-30)
+            time += 10
+        while time < 80:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(-0.9*time+74)
+            time += 10
+        while time < 120:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(2)
+            time += 10
+        while time < 140:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(0.9*time-106)
+            time += 10
+        while time < 160:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(-0.9*time+146)
+            time += 10
+        while time < 200:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(2)
+            time += 10
+        df = pd.DataFrame(jobs_data, columns=['time', 'job_count'])
+        df.to_csv('bursty.csv')
+
+    def incremental(self):
+        jobs_data = {'time': [], 'job_count': []}
+        time = 0
+        while time < 200:
+            jobs_data['time'].append(time)
+            count = random.randint(0.1*time, 0.1*(time+20))
+            jobs_data['job_count'].append(count)
+            time += 10
+        df = pd.DataFrame(jobs_data, columns=['time', 'job_count'])
+        df.to_csv('incremental.csv')
+
+    def random(self):
+        jobs_data = {'time': [], 'job_count': []}
+        time = 0
+        while time < 200:
+            jobs_data['time'].append(time)
+            jobs_data['job_count'].append(random.randint(1, 20))
+            time += 10
+        df = pd.DataFrame(jobs_data, columns=['time', 'job_count'])
+        df.to_csv('random.csv')
+
+    def main(self):
+        try:
+            workload_file = r'%s.csv' % self.workload
+            df = pd.read_csv(workload_file)
+        except Exception as ex:
+            print(ex)
 
 
 def newJob(data):
