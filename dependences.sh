7c53571614427b730ce4e87016fb808faf3a9f68
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-07-20 08:55:01 -0500

    update

--- dependences.sh
+++ dependences.sh
@@ -7,8 +7,8 @@ apt-get install libltdl7
 # install pip3
 apt-get install -y python3-pip python3-dev python3-setuptools xmlto asciidoc
 
-# install python packages for docker , flask, pyzmq, mongodb, cpuinfo
-pip3 install flask docker pyzmq pymongo py-cpuinfo flasgger
+# install python packages for docker , flask, pyzmq, mongodb, cpuinfo, pandas
+pip3 install flask docker pyzmq pymongo py-cpuinfo flasgger, pandas
 
 # check if docker exists
 docker_check=$(which docker)
