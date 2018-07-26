8843141f2c3fa0974472b79dd13aaa2b55f24cca
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-07-26 10:14:41 -0500

    update

--- Client/BurstyStressClient.py
+++ Client/BurstyStressClient.py
@@ -4,12 +4,32 @@
 
 import json
 import random
+import math
 from Client.Client import StressClient
 
 
 class BurstyStressClient(StressClient):
-    def __init__(self):
+    def __init__(self, lmda):
         super(StressClient).__init__()
+        self.lmda = lmda
 
     def feed_func(self, time_stamp):
-        random.expovariate()
+        return math.ceil(random.expovariate(self.lmda))
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-a', '--address', type=str, help='Front end node address.')
+    # parser.add_argument('-p', '--port', type=str, default='5001', help='Front end node port number.')
+    # args = parser.parse_args()
+    # fe_addr = args.address
+    # fe_port = args.port
+
+    try:
+        json_path = 'BurstyStressClientInfo.json'
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+        client = BurstyStressClient(lmda=data['lambda'])
+        client.feed_jobs()
+    except ValueError as er:
+        print(er)
