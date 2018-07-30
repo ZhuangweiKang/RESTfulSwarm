a6da354cbd36558fb2ee5d99b08fb7ba739641b6
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-07-29 23:03:42 -0500

    update

--- Discovery/Discovery.py
+++ Discovery/Discovery.py
@@ -25,90 +25,90 @@ class Discovery:
 
     def discovery(self):
         while True:
-            msg = self.socket.recv_string()
-            self.logger.info('Recv msg: %s' % msg)
-            self.socket.send_string('Ack')
-            worker_host = msg.split()[0]
-            msg = msg.split()[1]
-            task_name = msg
-            job_name = msg.split('_')[0]
-            job_col = mg.get_col(self.db, job_name)
+            try:
+                msg = self.socket.recv_string()
+                self.logger.info('Recv msg: %s' % msg)
+                self.socket.send_string('Ack')
+                worker_host = msg.split()[0]
+                msg = msg.split()[1]
+                task_name = msg
+                job_name = msg.split('_')[0]
+                job_col = mg.get_col(self.db, job_name)
 
-            # update job collection -- task status
-            filter_key = 'job_info.tasks.%s.container_name' % task_name
-            target_key = 'job_info.tasks.%s.status' % task_name
-            mg.update_doc(job_col, filter_key, task_name, target_key, 'Down')
+                # update job collection -- task status
+                filter_key = 'job_info.tasks.%s.container_name' % task_name
+                target_key = 'job_info.tasks.%s.status' % task_name
+                mg.update_doc(job_col, filter_key, task_name, target_key, 'Down')
 
-            # update job status if necessary
-            job_details = mg.find_col(job_col)[0]
-            flag = True
-            for job in job_details['job_info']['tasks']:
-                if job_details['job_info']['tasks'][job]['status'] != 'Down':
-                    flag = False
-            if flag:
-                mg.update_doc(job_col, 'job_name', job_name, 'status', 'Down')
-                mg.update_doc(job_col, 'job_name', job_name, 'end_time', time.time())
+                # update job status if necessary
+                job_details = mg.find_col(job_col)[0]
+                flag = True
+                for job in job_details['job_info']['tasks']:
+                    if job_details['job_info']['tasks'][job]['status'] != 'Down':
+                        flag = False
+                if flag:
+                    mg.update_doc(job_col, 'job_name', job_name, 'status', 'Down')
+                    mg.update_doc(job_col, 'job_name', job_name, 'end_time', time.time())
 
-            self.logger.info('Updating Job collection.')
+                self.logger.info('Updating Job collection.')
 
-            # get the resource utilization of the 'Down' container
-            job_info = mg.find_col(job_col)[0]
-            cores = job_info['job_info']['tasks'][task_name]['cpuset_cpus']
-            cores = cores.split(',')
-            memory = job_info['job_info']['tasks'][task_name]['mem_limit']
-            self.logger.info('Collecting resources from down containers.')
+                # get the resource utilization of the 'Down' container
+                job_info = mg.find_col(job_col)[0]
+                cores = job_info['job_info']['tasks'][task_name]['cpuset_cpus']
+                cores = cores.split(',')
+                memory = job_info['job_info']['tasks'][task_name]['mem_limit']
+                self.logger.info('Collecting resources from down containers.')
 
-            # update WorkersInfo collection
-            # update cores info
-            for core in cores:
-                target_key = 'CPUs.%s' % core
-                print(target_key)
-                mg.update_doc(self.workers_info, 'hostname', worker_host, target_key, False)
-                self.logger.info('Release core %s status in worker %s' % (target_key, worker_host))
+                # update WorkersInfo collection
+                # update cores info
+                for core in cores:
+                    target_key = 'CPUs.%s' % core
+                    print(target_key)
+                    mg.update_doc(self.workers_info, 'hostname', worker_host, target_key, False)
+                    self.logger.info('Release core %s status in worker %s' % (target_key, worker_host))
 
-            # update memory info
-            worker_info = mg.filter_col(self.workers_info, 'hostname', worker_host)
-            free_memory = worker_info['MemFree']
-            memory = float(memory.split('m')[0])
-            free_memory = float(free_memory.split('m')[0])
-            updated_memory = memory + free_memory
-            updated_memory = str(updated_memory) + 'm'
-            mg.update_doc(self.workers_info, 'hostname', worker_host, 'MemFree', updated_memory)
-            self.logger.info('Updating memory resources in WorkersInfo collection.')
+                # update memory info
+                worker_info = mg.filter_col(self.workers_info, 'hostname', worker_host)
+                free_memory = worker_info['MemFree']
+                memory = float(memory.split('m')[0])
+                free_memory = float(free_memory.split('m')[0])
+                updated_memory = memory + free_memory
+                updated_memory = str(updated_memory) + 'm'
+                mg.update_doc(self.workers_info, 'hostname', worker_host, 'MemFree', updated_memory)
+                self.logger.info('Updating memory resources in WorkersInfo collection.')
 
-            # update worker resource collection
-            mg.update_workers_resource_col(self.workers_info, worker_host, self.workers_resource_info)
-            self.logger.info('Updated WorkersResourceInfo collection, because some cores are released.')
+                # update worker resource collection
+                mg.update_workers_resource_col(self.workers_info, worker_host, self.workers_resource_info)
+                self.logger.info('Updated WorkersResourceInfo collection, because some cores are released.')
 
-            # update job collection -- cpuset_cpus
-            target_key = 'job_info.tasks.%s.cpuset_cpus' % task_name
-            mg.update_doc(job_col, filter_key, task_name, target_key, '')
-            self.logger.info('Updated Job collection. Released used cores.')
+                # update job collection -- cpuset_cpus
+                target_key = 'job_info.tasks.%s.cpuset_cpus' % task_name
+                mg.update_doc(job_col, filter_key, task_name, target_key, '')
+                self.logger.info('Updated Job collection. Released used cores.')
 
-            # update job collection -- mem_limit
-            target_key = 'job_info.tasks.%s.mem_limit' % task_name
-            mg.update_doc(job_col, filter_key, task_name, target_key, '')
-            self.logger.info('Updated Job collection. Released used memory.')
+                # update job collection -- mem_limit
+                target_key = 'job_info.tasks.%s.mem_limit' % task_name
+                mg.update_doc(job_col, filter_key, task_name, target_key, '')
+                self.logger.info('Updated Job collection. Released used memory.')
+            except Exception as ex:
+                self.logger.error(ex)
 
 
 def main():
     os.chdir('/home/%s/RESTfulSwarmLM/Discovery' % utl.getUserName())
 
-    try:
-        with open('DiscoveryInit.json') as f:
-            data = json.load(f)
-        mongo_addr = data['mongo_addr']
-        mongo_port = data['mongo_port']
 
-        discovery = Discovery(mongo_addr, mongo_port)
-        discovery.logger.info('Initialized Discovery block.')
+    with open('DiscoveryInit.json') as f:
+        data = json.load(f)
+    mongo_addr = data['mongo_addr']
+    mongo_port = data['mongo_port']
 
-        discovery.discovery()
+    discovery = Discovery(mongo_addr, mongo_port)
+    discovery.logger.info('Initialized Discovery block.')
 
-        os.chdir('/home/%s/RESTfulSwarmLM/ManagementEngine' % utl.getUserName())
-    except Exception as ex:
-        with open('debug.txt', 'w') as f:
-            f.write(ex)
+    discovery.discovery()
+
+    os.chdir('/home/%s/RESTfulSwarmLM/ManagementEngine' % utl.getUserName())
 
 
 if __name__ == '__main__':
