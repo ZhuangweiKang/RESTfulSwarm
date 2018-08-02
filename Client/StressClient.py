506a62068af25a768eeb5f307df8f708cc65699c
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-08-02 10:42:39 -0500

    update

--- Client/StressClient.py
+++ Client/StressClient.py
@@ -4,6 +4,7 @@
 
 from abc import ABCMeta, abstractmethod
 import requests
+import random
 import time
 import json
 
@@ -59,10 +60,19 @@ class StressClient(object):
         job['job_info']['network']['subnet'] = self.subnet
         for i in range(self.task_count):
             task_name = job_name + '_task' + str(i+1)
+
+            # For testing switch scheduler
+            node_choice = ['testswarm1', 'testswarm2']
+            node = node_choice[random.randint(0, 1)]
+            cores = []
+            for i in range(4):
+                cores.append(str(random.randint(0, 47)))
+            cpuset_cpus = ','.join(cores)
+
             image = self.image_name
             cpu_count = self.task_cores
             mem_limit = str(self.task_mem) + 'm'
-            task = self.Task(task_name, image, cpu_count, mem_limit)
+            task = self.Task(task_name, image, cpu_count, mem_limit, node=node, cpuset_cpus=cpuset_cpus)
             task = task.generate_task()
             job['job_info']['tasks'].update({task_name: task})
         return job
@@ -72,15 +82,27 @@ class StressClient(object):
         return 0
 
     def feed_jobs(self, session_id):
-        max_time = 20 * self.time_interval
-        time_index = 0
-        while time_index <= max_time:
-            job_count = self.feed_func(time_index)
-            for i in range(job_count):
-                job_name = 'job' + str(int(time.time() * 1000)) + '-' + session_id
-                self.newJob(self.generate_job(job_name))
-            time.sleep(self.time_interval)
-            time_index += self.time_interval
+        max_time = 3 * self.time_interval
+
+        def feed():
+            time_index = 0
+            while time_index <= max_time:
+                job_count = self.feed_func(time_index)
+                for i in range(job_count):
+                    job_name = 'job' + str(int(time.time() * 1000)) + '-' + session_id
+                    self.newJob(self.generate_job(job_name))
+                time.sleep(self.time_interval)
+                time_index += self.time_interval
+
+        feed()
+        time.sleep(5)
+
+        # Switch scheduler
+        url = 'http://%s:%s/RESTfulSwarm/FE/switchScheduler/no-scheduler' % (self.fe_addr, self.fe_port)
+        print(requests.post(url=url).content)
+
+        time.sleep(5)
+        feed()
 
     def newJob(self, data):
         url = 'http://%s:%s/RESTfulSwarm/FE/requestNewJob' % (self.fe_addr, self.fe_port)
