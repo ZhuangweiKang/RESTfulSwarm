cfe18688a5ffff9a27a799e47449877658d482a2
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-09-08 16:29:41 -0500

    update database creation command

--- dependences.sh
+++ dependences.sh
@@ -71,6 +71,7 @@ install_db(){
     echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
     apt-get update
     apt-get install -y mongodb-org
+    systemctl daemon-reload
     systemctl start mongod
     systemctl status mongod
     systemctl enable mongod
