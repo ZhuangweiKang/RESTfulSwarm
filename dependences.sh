651e88e018d822006361322bf98daf924e7d9a05
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-09-08 11:22:19 -0500

    update database creation command

--- dependences.sh
+++ dependences.sh
@@ -67,8 +67,8 @@ install_criu(){
 
 install_db(){
     # install mongodb 3.2 on ubuntu 16.04
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
     apt-get update
     apt-get install -y mongodb-org
     systemctl start mongod
@@ -78,7 +78,7 @@ install_db(){
 
     # create user and db
     echo "use $3; db.createUser( { user: \"$1\", pwd: \"$2\", roles: [ { role: \"readWrite\", db: \"$3\" } ] } );" > initdb.js
-    mongo -u "$1" -p "$2" --authenticationDatabase "$3" < initdb.js
+    mongo < initdb.js
 
     sleep 1
 
