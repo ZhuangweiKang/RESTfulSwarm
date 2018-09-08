7b2b98f2779cc3a82c00ba924f806ad6fa01c6cc
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-09-08 16:06:06 -0500

    update database creation command

--- dependences.sh
+++ dependences.sh
@@ -69,15 +69,15 @@ install_db(){
     # install mongodb 3.2 on ubuntu 16.04
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-    apt-get updateg
-    apt-get install mongodb-org
-    systemctl start mongod
+    apt-get update
+    apt-get install -y mongodb-org
+    service mongod start
     systemctl status mongod
     systemctl enable mongod
     systemctl restart mongod
 
     # create user and db
-    mongo --eval "use admin; db.createUser( { user: \"$1\", pwd: \"$2\", roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" } ] } );"
+    mongo --host 127.0.0.1:27017 --eval "use admin; db.createUser( { user: \"$1\", pwd: \"$2\", roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" } ] } );"
 
     sleep 1
 
