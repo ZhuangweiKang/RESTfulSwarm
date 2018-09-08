8863a764ba760662847fff0da34cc41914c2d44a
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-09-08 15:24:25 -0500

    update database creation command

--- dependences.sh
+++ dependences.sh
@@ -70,7 +70,7 @@ install_db(){
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
     apt-get updateg
-    apt-get install -y mongodb-org
+    apt-get install mongodb-org
     systemctl start mongod
 
     sleep 3
@@ -87,7 +87,8 @@ install_db(){
     sleep 1
 
      # reconfigure mongodb to allow remote access
-    mv ./mongod.conf /etc/mongod.conf
+    # mv ./mongod.conf /etc/mongod.conf
+    echo "security:     authorization: \"enabled\"" >> /etc/mongod.conf
     systemctl restart mongod
     systemctl status mongod
 
