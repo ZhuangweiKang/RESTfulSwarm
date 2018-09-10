d5d3c5396917dc802cd298adb463641ff038b6fd
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-09-09 20:54:22 -0500

    prune network is not working

--- Discovery/Discovery.py
+++ Discovery/Discovery.py
@@ -43,12 +43,13 @@ class Discovery(object):
                 target_key = 'job_info.tasks.%s.status' % task_name
                 mg.update_doc(job_col, filter_key, task_name, target_key, 'Down')
 
-                # update job status if necessary
+                # update job status if all tasks are down
                 flag = True
                 for task in job_details['job_info']['tasks']:
                     if job_details['job_info']['tasks'][task]['status'] != 'Down':
-                        print(job_details['job_info']['tasks'][task]['container_name'])
                         flag = False
+                    else:
+                        print(job_details['job_info']['tasks'][task]['container_name'])
                 if flag:
                     mg.update_doc(job_col, 'job_name', job_name, 'status', 'Down')
                     mg.update_doc(job_col, 'job_name', job_name, 'end_time', time.time())
